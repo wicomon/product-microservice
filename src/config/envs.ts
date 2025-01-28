48d771dd74838a6a29b8c32eb313c87511d565e0
wicomon <wcv.94@hotmail.com>
2025-01-28 18:07:34 -0500

    feat: NAT server implemented

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,14 +4,21 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+
+  NATS_SERVERS: string[];
 }
 
 const envsSchema = joi.object({
   PORT: joi.number().required(),
   DATABASE_URL: joi.string().required(),
-}).unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+  NATS_SERVERS: joi.array().items(joi.string()).required(),
+}).unknown(true);
+// console.log({nats: process.env.NATS_SERVERS?.split(',')})
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -22,4 +29,5 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   databaseUrl: envVars.DATABASE_URL,
+  natsServers: envVars.NATS_SERVERS,
 }
